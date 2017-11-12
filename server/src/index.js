d625be9e7e1d11b070c7dcd7be1ea814c68e4946
scyclow <spike716@gmail.com>
2017-11-12 14:52:46 -0500

    add babel stuff. fix gitignore

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,11 @@
-const express = require('express')
-const PORT = 3335
+import express from 'express';
 
-const app = express()
+export const PORT = 3335;
+
+export const app = express();
 
 app.use((req, res) => {
   console.log('bleh>>>>>>>>>>>>>>')
   res.send('w0000_______00tttttt')
 })
 
-
-exports.app = app
-exports.PORT = PORT
